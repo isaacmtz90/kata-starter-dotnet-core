a5080cd967261d6a7c29555f8eae81478d005b39
Isaac Martinez <imtz90b@gmail.com>
2019-08-05 14:23:47 -0600

    Pong

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,10 +1,18 @@
+using System;
+
 namespace Kata
 {
     public class Calculator
     {
         public int Add(string s="")
         {
+            if (string.IsNullOrEmpty(s))
+            {
             return 0;
+               
+            }
+            return Int32.Parse(s);
+            
         }
     }
 }
