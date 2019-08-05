6f09c29837240e83e8a05431c8f78c7c562fada1
Byron Sommardahl <byron@acklenavenue.com>
2019-08-05 15:26:30 -0500

    Ping

--- src/Kata.Spec/when_input_is_empty.cs
+++ src/Kata.Spec/when_input_is_empty.cs
@@ -16,10 +16,7 @@ namespace Kata.Spec
 
     public class when_input_is_one_number
     {
-        Establish _context = () =>
-        {
-            _systemUnderTest = new Calculator();
-        };
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
 
         Because of = () => { _result = _systemUnderTest.Add("1"); };
 
@@ -30,19 +27,28 @@ namespace Kata.Spec
 
     public class when_input_is_two_numbers
     {
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = _systemUnderTest.Add("3,4"); };
+
+        It should_return_the_sum_of_those_numbers = () => { _result.Should().Be(7); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
+
+    public class when_summing_an_unknown_amount_of_numbers
+    {
         Establish _context = () =>
         {
             _systemUnderTest = new Calculator();
         };
 
-        Because of = () => { _result = _systemUnderTest.Add("3,4"); };
+        Because of = () => { _result = _systemUnderTest.Add("1,2,3"); };
 
-        It should_return_the_sum_of_those_numbers = () => { _result.Should().Be(7); };
-        private static Calculator _systemUnderTest;
-        private static int _result;
+        It should_return_sum_of_all = () => { _result.Should().Be(6); };
+        static Calculator _systemUnderTest;
+        static int _result;
     }
-    
-//3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 //4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
 //5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
 //6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,18 +1,16 @@
 using System;
+using System.Linq;
 
 namespace Kata
 {
     public class Calculator
     {
-        public int Add(string s="")
+        public int Add(string s = "")
         {
-            if (string.IsNullOrEmpty(s))
-            {
-            return 0;
-               
-            }
-            return Int32.Parse(s);
-            
+            if (string.IsNullOrEmpty(s)) return 0;
+            var numbers = s.Split(",").Select(int.Parse).ToArray();
+            if (numbers.Length == 1) return numbers.First();
+            return numbers.First()+numbers.Last();
         }
     }
 }
