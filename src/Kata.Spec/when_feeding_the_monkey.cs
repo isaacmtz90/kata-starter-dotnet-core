91b33dc7b00e58caa906aaaef5be4d13f340f533
Byron Sommardahl <byron@acklenavenue.com>
2019-08-12 11:55:03 -0500

    Ping

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -91,36 +91,50 @@ namespace Kata.Spec
 
     public class when_input_includes_a_negative_number
     {
-        Establish _context = () =>
-        {
-            _systemUnderTest = new Calculator();
-        };
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
 
         Because of = () => { _result = Catch.Exception(() => _systemUnderTest.Add("1,2,-3")); };
 
-        It should_throw_an_exception_with_that_number = () => { _result.Message.Should().Be("negatives not allowed: -3"); };
+        It should_throw_an_exception_with_that_number = () =>
+        {
+            _result.Message.Should().Be("negatives not allowed: -3");
+        };
+
         static Calculator _systemUnderTest;
         static Exception _result;
     }
 
     public class when_having_multiple_negatives
     {
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = Catch.Exception(() => _systemUnderTest.Add("1,-2,-3")); };
+
+        It should_return_an_exception_listing_them = () =>
+        {
+            _result.Message.Should().Be("negatives not allowed: -2, -3");
+        };
+
+        static Exception _result;
+        static Calculator _systemUnderTest;
+    }
+
+    public class when_using_numbers_larger_than_1000
+    {
         Establish _context = () =>
         {
             _systemUnderTest = new Calculator();
         };
 
-        Because of = () => { _result = Catch.Exception(() => _systemUnderTest.Add("1,-2,-3")); };
+        Because of = () => { _result = _systemUnderTest.Add("1,2,1001,3"); };
 
-        It should_return_an_exception_listing_them = () => { _result.Message.Should().Be("negatives not allowed: -2, -3"); };
-        private static Exception _result;
-        private static Calculator _systemUnderTest;
+        It should_return_the_sum_of_the_numbers_less_than_1001 = () => { _result.Should().Be(6); };
+        static Calculator _systemUnderTest;
+        static int _result;
     }
 }
 
 /*
-7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
-8. Given the user input contains multiple negative numbers mixed with positive numbers when calculating the sum then it should throw an exception "negatives not allowed: x, y, z" (where x, y, z are only the negative numbers). 
 9. Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
 10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1***2***3” should return 6)
 11. Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example “//[*][%]\n1*2%3” should return 6)
