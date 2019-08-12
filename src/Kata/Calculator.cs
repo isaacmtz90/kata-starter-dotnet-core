03584e03442a434a83beb12e41fab2e7a2b0ad60
Byron Sommardahl <byron@acklenavenue.com>
2019-08-12 11:50:01 -0500

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -7,9 +7,19 @@ namespace Kata
     {
         public int Add(string s = "")
         {
-            if(string.IsNullOrEmpty(s))
+            var input = s;
+            if(string.IsNullOrEmpty(input))
                 return 0;
-            var numbers = s.Split(new [] {",","\n"}, StringSplitOptions.None).Select((x) => int.Parse(x)).ToArray();
+            
+            var separator = new [] {",","\n"};
+            if (input.StartsWith("//"))
+            {
+                var parts = input.Split("\n");
+                input = parts[1];
+                separator = new[] {parts[0].Replace("//", "")};
+            }
+
+            var numbers = input.Split(separator, StringSplitOptions.None).Select((x) => int.Parse(x)).ToArray();
             return numbers.Sum();
         }
     }
