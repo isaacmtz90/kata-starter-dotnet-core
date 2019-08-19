fcb833e0d2850138dd4197325b9a1f14830a8497
Byron Sommardahl <byron@acklenavenue.com>
2019-08-19 14:36:30 -0500

    empty passing, one number failing.:

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -6,11 +6,11 @@ namespace Kata.Spec
     public class when_feeding_the_monkey
     {
         static Monkey _systemUnderTest;
-        
-        Establish context = () => 
+
+        Establish context = () =>
             _systemUnderTest = new Monkey();
 
-        Because of = () => 
+        Because of = () =>
             _systemUnderTest.Eat("banana");
 
         It should_have_the_food_in_its_belly = () =>
@@ -19,29 +19,32 @@ namespace Kata.Spec
 
     public class when_input_is_empty
     {
+        static Calculator _systemUnderTest;
+        static int _result;
+
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
+
+        Because of = () => { _result = _systemUnderTest.Add(); };
+
+        It should_return_zero = () => { _result.Should().Be(0); };
+    }
+
+    public class when_user_input_is_one_number
+    {
         Establish _context = () =>
         {
             _systemUnderTest = new Calculator();
         };
 
-        Because of = () => { _result = _systemUnderTest.Add(); };
+        Because of = () => { _result = _systemUnderTest.Add("1"); };
 
-        It should_return_zero = () => { _result.Should().Be(0); };
-        private static Calculator _systemUnderTest;
-        private static int _result;
-    }
-
-    internal class Calculator
-    {
-        public int Add()
-        {
-            return -1;
-        }
+        It should_return_the_same_number = () => { _result.Should().Be(1); };
+        static Calculator _systemUnderTest;
+        static int _result;
     }
 }
 
 
-// 1. Given the user input is empty when calculating the sum then it should return zero.
 // 2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
 // 3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 // 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
