fc68756c9d195823afce94aeef933daf37715c64
Isaac Martinez <imtz90b@gmail.com>
2019-08-26 12:27:55 -0600

    when multiple negatives passing, larger than 1000 failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -20,7 +20,7 @@ namespace Kata
             var negatives = numbers.Where(x => x < 0);
             if (negatives.Any())
             {
-                throw new Exception("negatives not allowed: -3");
+                throw new Exception($"negatives not allowed: {string.Join(", ",negatives)}");
             }
             
             if (numbers.Count() == 1)
