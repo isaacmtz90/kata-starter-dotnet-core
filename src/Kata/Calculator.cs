ad297db2aff472e7764ffa1c7ebcf0066fbc9692
Isaac Martinez <imtz90b@gmail.com>
2019-09-20 08:34:24 -0600

    newline delimiter passing, custom delimiter failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 
 namespace Kata
@@ -9,7 +10,7 @@ namespace Kata
             if(string.IsNullOrEmpty(s))
                 return 0;
 
-            var numbers = s.Split(",").Select((int.Parse));
+            var numbers = s.Split( new []{",", "\n"}, StringSplitOptions.None ).Select((int.Parse));
             if (numbers.Count() == 1)
             {
                 
